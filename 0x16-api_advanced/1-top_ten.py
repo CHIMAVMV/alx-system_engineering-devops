600fbc4f720dd45f8b2d8894a969fb531b68c6e5
Chima Gideon Nweke <nweke_chima@yahoo.com>
2024-08-14 04:12:07 +0000

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,31 @@
-#1/usr/bin/python3
-"""using reddit API to get 10 hot posts """
-import requests
+#!/usr/bin/python3
+"""how many subs? """
 
-def top_ten(subreddit):
-    """Get ten hot post."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'user-agent': 'request'}
-    response = request.get(url, headers=headers, allow_redirrects=false)
-    
-    if response.status_code != 200:
-        print(None)
-        return 
-    data = response.json().get("data").get("children")
-    top_10_posts = "\n".json(post.get("data").get("title") for post in data)
-    print(top_10_posts)
+
+def number_of_subscribers(subreddit):
+    """ Returns subscriber count of subreddit or 0 """
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return 0
+
+    data = js.get("data")
+
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
+
+    return 0
